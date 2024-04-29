bfa22b0a1c12aced973f2297c23214e51a541b90
Viraj Shah <98512668+ShahViraj11@users.noreply.github.com>
2024-04-29 19:09:54 -0400

    Time: 59 ms (65.57%), Space: 17.7 MB (41.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        oldMap = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in oldMap:
+                return [oldMap[diff], i]
+            else:
+                oldMap[n] = i
+        return
+
+
+        
