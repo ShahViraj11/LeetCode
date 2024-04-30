8f0f3106512f07702168096e064cfe0636df0b12
Viraj Shah <98512668+ShahViraj11@users.noreply.github.com>
2024-04-30 12:17:03 -0400

    Time: 56 ms (45.91%), Space: 16.6 MB (64.44%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        y = str(x)
+        if y == y[::-1]:
+            return True
+        else:
+            return False
+
+
+        
